5a7c39ccdd4c5380d9658a98b2d5290c72954324
thatzita <robinudehn@gmail.com>
2019-04-07 16:35:48 +0200

    Updates

---
+++ precache-manifest.7e25ed2955dc6b8f1a928c4aac8ca557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "125f668010062d60f9f0",
+    "url": "/portfolio/static/js/main.882c6a35.chunk.js"
+  },
+  {
+    "revision": "c89c42c3d411929d394d",
+    "url": "/portfolio/static/js/2.bea7a9eb.chunk.js"
+  },
+  {
+    "revision": "125f668010062d60f9f0",
+    "url": "/portfolio/static/css/main.c08cb8e6.chunk.css"
+  },
+  {
+    "revision": "fe0e6bd60d8b6a474823e50187972774",
+    "url": "/portfolio/index.html"
+  }
+];
